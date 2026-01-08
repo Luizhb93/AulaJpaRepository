f375073594c567767446ecc38f01d91c57624ba0
Luiz Brito <luiz.brito@inema.ba.gov.br>
2026-01-08 16:27:45 -0300

    Implementa filtro de busca por salario

--- src/main/java/br/com/luiz/aulajpa/repositories/UserRepository.java
+++ src/main/java/br/com/luiz/aulajpa/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package br.com.luiz.aulajpa.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.luiz.aulajpa.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+	Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
 
 }
